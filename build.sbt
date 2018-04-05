e348e0fac5b52e5ea8df4d7ad6357b14f2b2cdfa
Brian Hsu <brianhsu.hsu@gmail.com>
2018-04-05 16:34:37 +0800

    add: Initial commit.

---
+++ build.sbt
@@ -0,0 +1,15 @@
+organization := "moe.brianhsu.gtd"
+
+scalaVersion := "2.12.5"
+
+scalacOptions := Seq("-deprecation")
+
+version := "0.1.0-SNAPSHOT"
+
+name := "GTD"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
+)
+
+
---
+++ src/test/scala/unitTest/AddUserSpec.scala
@@ -0,0 +1,59 @@
+package unitTest
+
+import scala.collection.mutable._
+import org.scalatest._
+
+object DAO {
+}
+
+trait DAO {
+}
+
+class DAOMock extends DAO {
+}
+
+class AddUser(implicit val dao: DAO) {
+}
+
+class AddUserSpec extends fixture.WordSpec with Matchers {
+
+  "AddUser" when {
+
+    "validation" should {
+      "throw exception if email is empty" in {implicit dao =>
+        pending
+      }
+
+      "throw exception if name is empty" in {implicit dao =>
+        pending
+      }
+
+      "throw exception if email is duplicate" in {implicit dao =>
+        pending
+      }
+
+    }
+
+    "save to database" should {
+      "set createTime / updateTime correctly" in { implicit dao =>
+        pending
+      }
+
+      "generate journal object" in { implicit dao =>
+        pending
+      }
+
+      "call DAO object to save" in { implicit dao =>
+        pending
+      }
+    }
+  }
+
+  type FixtureParam = DAO
+
+  override def withFixture(test: OneArgTest) = {
+    val dao = new DAOMock
+    test(dao)
+  }
+
+}
